cbc788d6219dd70273eb6dd395e4ca5d87b0ed83
dronefyagro <dronefyagro@gmail.com>
2026-08-22 22:46:08 -0300

    chore: refresh PWA cache for v1.1.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prescreve-agro-v2";
+const CACHE_NAME = "prescreve-agro-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
